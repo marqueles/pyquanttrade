df70492935b2cdf9b9361e5f97b217ce77c03430
Marcos Jimenez Gutierrez <marcosjimenez@Marcoss-MBP.home>
2022-03-01 12:58:47 +0100

    changing_things

--- pyquanttrade/features/functions.py
+++ pyquanttrade/features/functions.py
@@ -17,7 +17,7 @@ import numpy as np
 
 def line(value):
     """Line which can be used to cross with functions like RSI or MACD.   
-    Name: line\ *value*\ 
+    Name: line\ *value*
 
     :param value: Value of the line
     :type value: float
